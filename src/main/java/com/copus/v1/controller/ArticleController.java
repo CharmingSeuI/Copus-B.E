51c5ac67270646fc4697edf51cfb62d3721006f3
tlgur <begin1009@naver.com>
2022-09-21 17:40:22 +0900

    Gwoncha Article Method Frame Init

--- src/main/java/com/copus/v1/controller/ArticleController.java
+++ src/main/java/com/copus/v1/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.copus.v1.controller;
 
+import com.copus.v1.controller.dto.GwonchaResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/article")
 public class ArticleController {
+
+
+    @GetMapping("/gwoncha/{id}")
+    public GwonchaResponse getGwoncha(@PathVariable String id) {
+        GwonchaResponse gwonchaResponse = new GwonchaResponse();
+
+        /**
+         * Gwoncha Data For  Article
+         * @param Gwoncha id
+         * @return
+         * {
+         * 	"seojiId":'',
+         * 	"seojiTitle":'',
+         * 	"datas":[
+         *                {
+         * 			"gwonchaId":'',
+         * 			"gwonchaTitle":'',
+         * 			"munches":[
+         *                    {
+         * 						"muncheId":'',
+         * 						"muncheTitle":'',
+         *                    },...
+         * 				]
+         *        },...
+         * 	]
+         * }
+         */
+
+        return gwonchaResponse;
+    }
 }
