f36c9facc8b846aa0d2d4d6737e00be12cef9775
tlgur <begin1009@naver.com>
2022-09-21 17:12:02 +0900

    Preview Article Method Frame Init

--- src/main/java/com/copus/v1/controller/ArticleController.java
+++ src/main/java/com/copus/v1/controller/ArticleController.java
@@ -1,11 +1,42 @@
 package com.copus.v1.controller;
 
+import com.copus.v1.controller.dto.PreviewRequest;
+import com.copus.v1.controller.dto.PreviewResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/article")
 public class ArticleController {
+
+    @GetMapping("/preview")
+    public PreviewResponse getPreview(@ModelAttribute PreviewRequest previewRequest) {
+        PreviewResponse previewResponse = new PreviewResponse();
+
+        /**
+         * Data For Search Result Article
+         * @param Filter == 검색 필터 (total || bookTitle || authorName || gwonchaTitle || muncheTitle || content || dataId)
+         * @param keyword == 검색어
+         * @return {
+         * 	"count":'',
+         * 	"datas":[
+         *                {
+         * 			"seojiId":'',
+         * 			"seojiTitle": '',
+         * 			"authorName": '',
+         * 			"publishYear":'',
+         * 			"gwonchaId": '',  - if needContent is false, null
+         * 			"gwonchaTitle": '권차명',  - if needContent is false, null
+         * 			"muncheId": '',  - if needContent is false, null
+         * 			"muncheTitle": '문체명',  - if needContent is false, null
+         * 			"finalId":'', - if needContent is false, null
+         * 			"finalTitle":'최종정보 제목', - if needContent is false, null
+         * 			"contentPartition": '본문일부', //최대 200자  - if needContent is false, null
+         *        },...
+         * } if some field is null, make the block blanck
+         */
+
+        return previewResponse;
+    }
 }
