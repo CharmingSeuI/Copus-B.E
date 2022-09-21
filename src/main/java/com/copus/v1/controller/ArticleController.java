97c5603242e01512dd7b9c7b20d33f31b97ff262
tlgur <begin1009@naver.com>
2022-09-21 18:02:46 +0900

    Final Article Method Frame Init

--- src/main/java/com/copus/v1/controller/ArticleController.java
+++ src/main/java/com/copus/v1/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.copus.v1.controller;
 
+import com.copus.v1.controller.dto.FinalResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/article")
 public class ArticleController {
+
+    @GetMapping("/final/{id}")
+    public FinalResponse getFinal(@PathVariable String id) {
+        FinalResponse finalResponse = new FinalResponse();
+
+        /**
+         * Final Data for Article
+         * @return{
+         * 	"seojiId":'',
+         * 	"seojiTitle":'',
+         * 	"gwonchaId":'',
+         * 	"gwonchaTitle":'',
+         * 	"muncheId":'',
+         * 	"muncheTitle":'',
+         * 	"final":{
+         * 		"title":'',
+         * 		"content":'',
+         *        }
+         * }
+         */
+
+        return finalResponse;
+    }
 }
