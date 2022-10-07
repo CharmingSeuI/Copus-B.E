4a164e03e931a56b6785a02ccdabc34221e8e275
willjsw <104188728+willjsw@users.noreply.github.com>
2022-10-07 10:56:52 +0900

    Category-Child-GetChildLevelData(ByBookName/ByAuthorName) & related files updated

---
+++ src/test/java/com/copus/v1/service/category/child/GetChildLevelDataTest.java
@@ -0,0 +1,11 @@
+package com.copus.v1.service.category.child;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class GetChildLevelDataTest {
+    //테스트 전
+}
