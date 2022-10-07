d2ad53ae842daa4c8235fa18a86e6482550d44f9
willjsw <104188728+willjsw@users.noreply.github.com>
2022-10-07 21:23:19 +0900

    error in GetChildLevelData fixed(ConsonantRangeSet issue)

--- src/main/java/com/copus/v1/service/ConsonantRangeSet.java
+++ src/main/java/com/copus/v1/service/ConsonantRangeSet.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Service
 public class ConsonantRangeSet {
     public ArrayList<String> consonantRangeSet(String consonant){
-        final String finalConsonant[] ={"가","나","다","라","마","바","사","아","자","차","카","타","파","하"};
+        final String finalConsonant[] ={"가","나","다","라","마","바","사","아","자","차","카","타","파","하","힣"};
         List<String> consonantList = Arrays.asList(finalConsonant);
         ArrayList<String> result= new ArrayList<>();
 
         String consonant1;
         String consonant2;
 
-        if (consonantList.contains(consonant) && consonant.equals("힣")){
+        if (consonantList.contains(consonant)){
             consonant1 = consonant;
             int consonant1Index = consonantList.indexOf(consonant1);
             consonant2 = consonantList.get(consonant1Index + 1);
--- src/test/java/com/copus/v1/service/category/child/GetChildLevelDataTest.java
+++ src/test/java/com/copus/v1/service/category/child/GetChildLevelDataTest.java
@@ -1,10 +1,23 @@
 package com.copus.v1.service.category.child;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 class GetChildLevelDataTest {
+
+    @Autowired
+    private GetChildLevelDataByAuthorName getChildLevelDataByAuthorName;
+    @Autowired
+    private GetChildLevelDataByBookTItle getChildLevelDataByBookTItle;
+
+    @Test
+    void child(){
+        System.out.println(getChildLevelDataByAuthorName.getChildDataByAuthorName("자",-1L));
+        System.out.println(getChildLevelDataByBookTItle.getChildDataByBookTitle("아",0L));
+    }
 
 }
